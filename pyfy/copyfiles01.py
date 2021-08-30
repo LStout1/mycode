d7a9cf7d1f50a0fc98282355d9394c8ea5fa5204
Leonard <LStout1@users.noreply.github.com>
2021-08-30 22:12:27 +0000

    reviewing how to manipulate JSON data with python

---
+++ pyfy/copyfiles01.py
@@ -0,0 +1,8 @@
+"""Coping files on the local system"""
+import shutil
+
+def main():
+    shutil.copyfile('/home/student/mycode/pyfy/ciscoex.json', '/home/student/mycode/pyfy/ciscoex.json.bkup')
+    # that was easy!
+
+main()
